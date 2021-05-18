24ec941f963fe0a6cc6e6c610ac66375022b9ab9
suryadi <68637256+suryadi445@users.noreply.github.com>
2021-05-18 16:45:50 +0700

    membuat file pagination pada config untuk memisahkan style dari pagination dengan controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -39,47 +39,13 @@ class Admin extends CI_Controller
 
     public function semua_promo()
     {
-        // $data['all_promo']      = $this->Admin_model->get_all_promo();
-
         // load library
         $this->load->library('pagination');
 
         // config
-        $config['base_url'] = 'http://localhost/company_profile/admin/semua_promo/';
         $config['total_rows'] = $this->Admin_model->count_rows();
         $config['per_page'] = 2;
 
-        // style pagination
-        $config['full_tag_open']    = '<nav"><ul class="pagination justify-content-center"';
-        $config['full_tag_close']   = '</ul></nav>';
-        // first pagination
-        $config['first_link']       = 'First';
-        $config['first_tag_open']   = '<li class="page-item">';
-        $config['first_tag_close']   = '</li>';
-
-        // last pagination
-        $config['last_link'] = 'Last';
-        $config['last_tag_open']   = '<li class="page-item">';
-        $config['last_tag_close']   = '</li>';
-
-        // next pagination
-        $config['next_link'] = '>>';
-        $config['next_tag_open']   = '<li class="page-item">';
-        $config['next_tag_close']   = '</li>';
-
-        // prev pagination
-        $config['prev_link'] = '<<';
-        $config['prev_tag_open']   = '<li class="page-item">';
-        $config['prev_tag_close']   = '</li>';
-
-        // link yang aktif
-        $config['cur_tag_open']   = '<li class="page-item active"><a class="page-link" href="#">';
-        $config['cur_tag_close']   = '</a></li>';
-
-        $config['num_tag_open']   = '<li class="page-item">';
-        $config['num_tag_close']   = '</li>';
-
-        $config['attributes'] = array('class' => 'page-link');
         // inisisalisasi
         $this->pagination->initialize($config);
 
