c5e3263c1ac1eca28b8b939e3edc17c8e5dc466b
suryadi <68637256+suryadi445@users.noreply.github.com>
2021-05-30 16:32:55 +0700

    membuat insert ke email dr halaman hubungi kami

--- application/controllers/Hubungi_kami.php
+++ application/controllers/Hubungi_kami.php
@@ -3,12 +3,47 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Hubungi_kami extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('Admin_model');
+    }
+
     public function index()
     {
-        $data['judul'] = 'Hubungi Kami';
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/navbar', $data);
-        $this->load->view('footer/hubungi_kami');
-        $this->load->view('templates/footer');
+        $this->form_validation->set_rules('nama', 'Nama', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('phone', 'Phone', 'trim|required|numeric');
+        $this->form_validation->set_rules('kategori', 'Kategori', 'trim|required');
+        $this->form_validation->set_rules('pesan', 'Pesan', 'trim|required|max_length[1000]');
+
+        $nama           = $this->input->post('nama');
+        $email          = $this->input->post('email');
+        $phone          = $this->input->post('phone');
+        $kategori       = $this->input->post('kategori');
+        $pesan          = $this->input->post('pesan');
+
+        $data               = [
+            'nama'          => $nama,
+            'email'         => $email,
+            'phone'         => $phone,
+            'kategori'      => $kategori,
+            'pesan'         => $pesan,
+            'tanggal_pesan' => date('yyyy-mm-dd')
+        ];
+
+        if ($this->form_validation->run() == false) {
+            $data['judul'] = 'Hubungi Kami';
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/navbar');
+            $this->load->view('footer/hubungi_kami', $data);
+            $this->load->view('templates/footer');
+        } else {
+            // $this->Admin_model->insert('email', $data);
+            $this->session->set_flashdata('sukses', 'Terima kasih, pesan Anda sudah terkirim');
+
+            redirect('hubungi_kami');
+        }
     }
 }
--- application/views/footer/hubungi_kami.php
+++ application/views/footer/hubungi_kami.php
@@ -6,6 +6,9 @@
                 <h1 id="promo_title">Hubungi Kami</h1>
             </div>
         </div>
+        <?php if ($this->session->flashdata()) : ?>
+            <div class="flash" data-id="<?= $this->session->flashdata('sukses') ?>"></div>
+        <?php endif ?>
         <div class="row text-justify" id="gambar_contact">
             <div class="col-12">
                 <div class="row justify-content-between">
@@ -26,46 +29,51 @@
                     </div>
                     <div class="col-6">
                         <div class="card shadow-sm p-3 mb-5 bg-white rounded" id="card_hub_kami">
-                            <form>
+                            <form action="<?= base_url('hubungi_kami/index') ?>" method="POST">
                                 <div class="container mt-3">
                                     <div class="form-group row">
                                         <label for="nama" class="col-sm-2 col-form-label font-weight-bold ">Nama</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" placeholder="Masukkan nama lengkap Anda" id="nama">
+                                            <input type="text" class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" value="<?= set_value('nama') ?>" placeholder="Masukkan nama lengkap Anda" id="nama" name="nama">
+                                            <div class="text-danger mb-n4"><?= form_error('nama') ?></div>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="email" class="col-sm-2 col-form-label font-weight-bold">Email</label>
                                         <div class="col-sm-10">
-                                            <input type="email" class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" placeholder="Masukkan email Anda" id="email">
+                                            <input type="email" class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" value="<?= set_value('email') ?>" placeholder="Masukkan email Anda" id="email" name="email">
+                                            <div class="text-danger mb-n4"><?= form_error('email') ?></div>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="phone" class="col-sm-2 col-form-label font-weight-bold">Phone</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" placeholder="Masukkan nomor telepon Anda" id="phone">
+                                            <input type="text" class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" value="<?= set_value('phone') ?>" placeholder="Masukkan nomor telepon Anda" id="phone" name="phone">
+                                            <div class="text-danger mb-n4"><?= form_error('phone') ?></div>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="phone" class="col-sm-2 col-form-label font-weight-bold">Kategori</label>
                                         <div class="col-sm-10">
-                                            <select class="form-control font-weight-bold">
+                                            <select class="form-control font-weight-bold" name="kategori">
                                                 <option class="font-weight-bold">Pilih Kategori</option>
-                                                <option class="font-weight-bold">Keluhan</option>
-                                                <option class="font-weight-bold">Saran</option>
-                                                <option class="font-weight-bold">Pertanyaan</option>
-                                                <option class="font-weight-bold">Sponsorship</option>
-                                                <option class="font-weight-bold">Lain-lain</option>
+                                                <option value="keluhan" class="font-weight-bold">Keluhan</option>
+                                                <option value="saran" class="font-weight-bold">Saran</option>
+                                                <option value="pertanyaan" class="font-weight-bold">Pertanyaan</option>
+                                                <option value="sponsorship" class="font-weight-bold">Sponsorship</option>
+                                                <option value="lain-lain" class="font-weight-bold">Lain-lain</option>
+                                                <div class="text-danger mb-n4"><?= form_error('kategori') ?></div>
                                             </select>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="pesan" class="col-sm-2 col-form-label font-weight-bold">Pesan</label>
                                         <div class="col-sm-10">
-                                            <textarea class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" placeholder="Masukkan pesan Anda" id="pesan" rows="3"></textarea>
+                                            <textarea class="form-control font-weight-bold shadow-sm p-3 bg-white rounded" placeholder="Masukkan pesan Anda" name="pesan" id="pesan" rows="3"><?= set_value('pesan') ?></textarea>
+                                            <div class="text-danger"><?= form_error('pesan') ?></div>
                                         </div>
                                     </div>
-                                    <button class="btn btn-danger float-right mb-3">Kirim Pesan</button>
+                                    <button type="submit" class="btn btn-danger float-right mb-3">Kirim Pesan</button>
                                 </div>
                             </form>
                         </div>
